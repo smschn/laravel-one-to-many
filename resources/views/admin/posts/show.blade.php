5c14253aa585e9c79c36a482eea86e5be33f5ba5
Luca Somaschini <somas99@gmail.com>
2022-10-11 19:40:30 +0200

    Added category to show view

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,11 @@
 
         <h1 class="fw-bold">Title: {{$post->title}}</h1>
         <h2 class="fw-bold">Slug: {{$post->slug}}</h2>
+        {{-- 
+            stampo la categoria usando la funzione <category()> del model <post>,
+            che si utilizza come se fosse un normale attributo\proprietà di <post>.
+        --}}
+        <h2 class="fw-bold">Category: {{($post->category)?$post->category->name:'No category'}}</h2>
         <div>
             <h3 class="fw-bold">Content:</h3>
             <p>{{$post->content}}</p>
